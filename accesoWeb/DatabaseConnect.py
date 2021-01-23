21f9a72f127a08d5e9029d0b73eac07d7776bb0f
verodaht <verodaht@gmail.com>
2021-01-23 17:51:53 +0100

    Se sube docker-compose para crear el docker con la bbdd.

--- accesoWeb/DatabaseConnect.py
+++ accesoWeb/DatabaseConnect.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient, errors
 
 # DEVUELVE LA CONEXION A LA BASE DE DATOS.
 def getConnection(databaseName):
-    mongoClient = MongoClient('localhost', port=27017)
+                                                        #THIS INFO IS TEMP
+    mongoClient = MongoClient('localhost', port=27017, username ='root', password='1234')
     db = mongoClient[databaseName]
     return db
